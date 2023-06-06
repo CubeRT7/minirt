052e13a9ffbb4a13b33649acfbddbf94ffba0639
iijung <987kmj4@naver.com>
2023-06-06 18:15:21 +0900

    fix: errno starts with 0 #9
    - In the terminal environment, errno is initialized to 0.
    - In the vscode(m1 mac) environment, errno is initialized to 1.
    - In the vscode(intel mac) environment, errno is initialized to 3.

--- src/miniRT.c
+++ src/miniRT.c
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 21:51:10 by minjungk          #+#    #+#             */
-/*   Updated: 2023/06/05 06:35:14 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/06/06 18:15:18 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ static int	exit_minirt(int exit_code)
 
 int	main(int argc, char **argv)
 {
+	errno = 0;
 	if (parse(get_scene(), argc, argv))
 		return (exit_minirt(ft_error(__func__, __FILE__, __LINE__, 0)));
 	return (exit_minirt(EXIT_SUCCESS));
