af43466fd492dc51f27b79b2c7110eb1ef236be2
sindydwns <sindydwns@naver.com>
2023-07-06 06:32:14 +0900

    refactory: add quadratic_formula_root functions to element's common util
    2차 함수 근의 공식 (짝수 공식)

--- src/Element/util/element_util.h
+++ src/Element/util/element_util.h
@@ -12,12 +12,22 @@
 
 #ifndef ELEMENT_UTIL_H
 # define ELEMENT_UTIL_H
+# define HAS_VALUE 0
+# define NO_VALUE 1
 # include "../common.h"
+
+typedef struct s_abc
+{
+	float	a;
+	float	b;
+	float	c;
+}	t_abc;
 
 extern t_point		get_ray_point(const t_ray *ray, float t);
 extern t_vector3	get_face_normal(t_ray ray, t_vector3 normal);
 extern int			range_in(float t, t_range range);
 extern int			range_not_in(float t, t_range range);
 extern int			close_to_zero(float f);
+extern int			quadratic_formula_root(t_abc abc, t_range range, float *x);
 
 #endif
