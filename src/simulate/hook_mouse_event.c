bdd137f3b73da5e8d4e0533723578780f31b7da8
minjungk <55140902+iijung@users.noreply.github.com>
2023-07-12 01:16:01 +0900

    Feature/camera (#35)
    * feat: rotate camera with mouse
    
    * fix: static world axis up view
    
    * fix: Fix conflict and  build error
    
    * fix: new version camera rotation!
    
    * data: new test file for check coordinate

--- src/simulate/hook_mouse_event.c
+++ src/simulate/hook_mouse_event.c
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 02:10:36 by minjungk          #+#    #+#             */
-/*   Updated: 2023/07/08 22:00:00 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/07/12 01:10:26 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,29 @@
 
 void	rotate_camera(void *param)
 {
-	t_world *const	world = param;
-	const float		delta = world->gui.mlx->delta_time;
-	t_vector3		tmp[2];
-	int				xy[2];
+	t_world *const		world = param;
+	t_camera *const		camera = world->camera;
+	int					xy[2];
+	t_vector3			v[4];
+	float				angle;
 
-	if (world->camera->obj.rotate_flag == 0)
+	enum e_type {UP, FRONT, RIGHT, NEW};
+	if (camera->obj.rotate_flag == 0)
 		return ;
-	if (world->camera->obj.rotate_flag == 2)
-		world->camera->obj.rotate_flag = 0;
+	if (camera->obj.rotate_flag == 2)
+		camera->obj.rotate_flag = 0;
 	mlx_get_mouse_pos(world->gui.mlx, &xy[0], &xy[1]);
-	tmp[0] = v3_normalize(v3_sub(vector3(xy[0], xy[1], 0),
-				vector3(world->camera->obj.cursor_xy[0],
-					world->camera->obj.cursor_xy[1], 0)));
-	tmp[1] = v3_cross_prod(tmp[0], world->camera->base.axis);
-	world->camera->base.axis = v3_rotate_axis(
-			world->camera->base.axis, tmp[1],
-			world->camera->obj.fov_radian * delta);
-	world->camera->base.axis = v3_normalize(world->camera->base.axis);
-	world->camera->obj.cursor_xy[0] = xy[0];
-	world->camera->obj.cursor_xy[1] = xy[1];
+	v[UP] = vector3(0, 1, 0);
+	v[FRONT] = v3_hadamard_prod(camera->base.axis, vector3(1, 0, 1));
+	v[RIGHT] = v3_normalize(v3_cross_prod(v[FRONT], v[UP]));
+	camera->base.axis = v3_rotate_axis(camera->base.axis, v[UP],
+			(camera->obj.cursor_pos.x - xy[0]) / world->gui.image->width);
+	v[NEW] = v3_rotate_axis(world->camera->base.axis, v[RIGHT],
+			(camera->obj.cursor_pos.y - xy[1]) / world->gui.image->height);
+	angle = v3_dot_prod(v3_normalize(v3_cross_prod(v[NEW], v[UP])), v[RIGHT]);
+	if (angle > 0)
+		camera->base.axis = v[NEW];
+	camera->obj.cursor_pos = vector3(xy[0], xy[1], 0);
 }
 
 void	hook_mouse_event(
@@ -44,20 +47,18 @@ void	hook_mouse_event(
 )
 {
 	t_world *const		world = param;
+	int					xy[2];
 
 	(void)mods;
+	mlx_get_mouse_pos(world->gui.mlx, &xy[0], &xy[1]);
 	if (button == MLX_MOUSE_BUTTON_RIGHT && action == MLX_PRESS)
 	{
 		world->camera->obj.rotate_flag = 1;
-		mlx_get_mouse_pos(world->gui.mlx,
-			&world->camera->obj.cursor_xy[0],
-			&world->camera->obj.cursor_xy[1]);
+		world->camera->obj.cursor_pos = vector3(xy[0], xy[1], 0);
 	}
 	else if (button == MLX_MOUSE_BUTTON_RIGHT && action == MLX_RELEASE)
 	{
 		world->camera->obj.rotate_flag = 2;
-		mlx_get_mouse_pos(world->gui.mlx,
-			&world->camera->obj.cursor_xy[0],
-			&world->camera->obj.cursor_xy[1]);
+		world->camera->obj.cursor_pos = vector3(xy[0], xy[1], 0);
 	}
 }
