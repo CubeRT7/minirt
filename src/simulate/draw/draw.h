356d7f10c414a51b0fafff3048deadbc3efbdda8
sindydwns <sindydwns@naver.com>
2023-07-12 06:16:40 +0900

    refactor: draw hit

--- src/simulate/draw/draw.h
+++ src/simulate/draw/draw.h
@@ -6,7 +6,7 @@
 /*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 04:00:10 by yonshin           #+#    #+#             */
-/*   Updated: 2023/07/12 04:02:29 by yonshin          ###   ########.fr       */
+/*   Updated: 2023/07/12 04:14:48 by yonshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 # include "Element/common.h"
 
 extern uint32_t	color_to_pixel(t_color c);
+extern int		hit(t_list *objs, t_ray *ray, t_range range, t_hit *record);
 
 #endif
