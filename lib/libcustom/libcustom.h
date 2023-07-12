886ec5a61ec67c79025704de13637d0e10f15d50
iijung <987kmj4@naver.com>
2023-07-13 00:40:58 +0900

    feat: float to str

--- lib/libcustom/libcustom.h
+++ lib/libcustom/libcustom.h
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 02:36:49 by minjungk          #+#    #+#             */
-/*   Updated: 2023/06/05 06:33:57 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/07/13 00:18:58 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 #  define DEBUG 0
 # endif
 
+extern char		*ft_ftoa(float f, int digit);
 extern int		ft_error(const char *func, char *file, int line, int sub_errno);
 extern float	ft_strtof(const char *nptr, char **endptr);
 extern void		ft_strarr_free(char **argv);
