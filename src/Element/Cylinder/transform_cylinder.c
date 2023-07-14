aac064c432c6c970f0cae2b6ce34f167d6b1bd91
sindydwns <sindydwns@naver.com>
2023-07-14 17:50:35 +0900

    cylinder rotation

--- src/Element/Cylinder/transform_cylinder.c
+++ src/Element/Cylinder/transform_cylinder.c
@@ -18,9 +18,14 @@ int	transform_cylinder(
 	enum e_transform_type type,
 	t_vector3 delta)
 {
-	(void)self;
-	(void)camera;
-	(void)type;
-	(void)delta;
+	const t_vector3 front = v3_normalize(vector3(camera->axis.x, 0, camera->axis.z));
+	const t_vector3 right = v3_normalize(v3_cross(front, vector3(0, 1, 0)));
+	const t_vector3 camera_up = v3_normalize(v3_cross(right, camera->axis));
+	if (type == (Rotation | X))
+		self->base.axis = v3_rotate_axis(self->base.axis, right, delta.x);
+	if (type == (Rotation | Y))
+		self->base.axis = v3_rotate_axis(self->base.axis, camera_up, delta.x);
+	if (type == (Rotation | Z))
+		self->base.axis = v3_rotate_axis(self->base.axis, v3_reverse(camera->axis), delta.x);
 	return (EXIT_SUCCESS);
 }
