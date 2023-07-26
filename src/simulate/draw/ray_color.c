c8d909f205c30f89023e16919717e655abcabec8
iijung <987kmj4@naver.com>
2023-07-26 17:36:27 +0900

    feat: render_mode specular

--- src/simulate/draw/ray_color.c
+++ src/simulate/draw/ray_color.c
@@ -6,7 +6,7 @@
 /*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 07:27:05 by yonshin           #+#    #+#             */
-/*   Updated: 2023/07/25 10:54:56 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/07/26 17:36:19 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,35 +43,38 @@ static t_vector3	_trim_bright(t_vector3 bright)
 static t_vector3	_get_light(
 	t_ray ray,
 	t_hit rec,
-	t_list *objs,
+	t_world *world,
 	t_light *light)
 {
-	t_ray		light_ray;
+	t_ray		to_light;
+	double		light_distance;
 	t_vector3	light_direction;
 	t_vector3	half_direction;
 	t_vector3	view_direction;
-	double		distance;
 	double		diffuse;
+	t_color		diffuse_color;
 	double		specular;
+	t_color		specular_color;
 
 	light_direction = v3_sub(light->base.position, rec.p);
-	distance = v3_magnitude(light_direction);
-	light_ray = (t_ray){rec.p, v3_normalize(light_direction)};
-	if (hit(objs, &light_ray, (t_range){DELTA, distance}, NULL))
+	light_distance = v3_magnitude(light_direction);
+	light_direction = v3_normalize(light_direction);
+	to_light = (t_ray){rec.p, light_direction};
+	if (hit(world->objs, &to_light, (t_range){DELTA, light_distance}, NULL))
 		return (v3_preset(V3_ZERO));
-	// diffuse_light
-	diffuse = v3_dot(light_ray.direction, rec.normal);
-	if (diffuse < 0)
-		return (v3_preset(V3_ZERO));
-	if (close_to_zero(distance))
-		distance = DELTA;
-	distance = sqrt(distance);
-	// specular_light
-	view_direction = v3_normalize(v3_sub(ray.origin, rec.p));
-	half_direction = v3_normalize(v3_sub(light_direction, view_direction));
-	specular = pow(v3_dot(half_direction, rec.normal), 100);
-	return (_trim_bright(v3_mul(light->base.color,
-				(diffuse + specular) * light->obj.ratio / distance)));
+	diffuse = 0;
+	if (world->render_mode & RENDER_DIFFUSE)
+		diffuse = fmax(0, v3_dot(light_direction, rec.normal));
+	diffuse_color = (v3_mul(light->base.color, light->obj.ratio * diffuse));
+	specular = 0;
+	if (world->render_mode & RENDER_SPECULAR)
+	{
+		view_direction = v3_normalize(v3_sub(ray.origin, rec.p));
+		half_direction = v3_normalize(v3_add(light_direction, view_direction));
+		specular = pow(fmax(0, v3_dot(half_direction, rec.normal)), 100);
+	}
+	specular_color = (v3_mul(light->base.color, light->obj.ratio * specular));
+	return (v3_add(diffuse_color, specular_color));
 }
 
 static t_color	_get_lights(t_ray *ray, t_world *world, t_hit *rec)
@@ -87,13 +90,12 @@ static t_color	_get_lights(t_ray *ray, t_world *world, t_hit *rec)
 	while (curr)
 	{
 		// TODO: empty light skip and none counting
-		brightness = (_get_light(*ray, *rec, world->objs, curr->content));
+		brightness = (_get_light(*ray, *rec, world, curr->content));
 		brightness_sum = v3_add(brightness_sum, brightness);
 		brightness_cnt++;
 		curr = curr->next;
 	}
-	brightness = _trim_bright(v3_mul(brightness_sum, 1.0 / brightness_cnt));
-	return (v3_hadamard(rec->color, brightness));
+	return (v3_mul(brightness_sum, 1.0 / brightness_cnt));
 }
 
 t_color	ray_color(t_ray *ray, t_world *world)
@@ -102,13 +104,14 @@ t_color	ray_color(t_ray *ray, t_world *world)
 
 	if (hit(world->objs, ray, (t_range){DELTA, BIGVALUE}, &rec) == 0)
 		return (v3_preset(V3_ZERO));
-	if ((world->render_mode & RENDER_ORIGINAL) == 0)
-		rec.color = v3_preset(V3_ZERO);
+	rec.color = v3_preset(V3_ZERO);
 	if (world->render_mode & RENDER_AMBIENT)
-		rec.color = alpha_blend(rec.color,
+		rec.color = v3_mul(
 				world->ambient_light->base.color,
 				world->ambient_light->obj.ratio);
 	if (world->render_mode & (RENDER_DIFFUSE | RENDER_SPECULAR))
-		rec.color = _get_lights(ray, world, &rec);
-	return (rec.color);
+		rec.color = _trim_bright(v3_add(rec.color, _get_lights(ray, world, &rec)));
+	if ((world->render_mode & RENDER_ORIGINAL) == 0)
+		return (rec.color);
+	return (alpha_blend(rec.color, rec.elem->color, 0.5));
 }
