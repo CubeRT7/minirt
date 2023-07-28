0f0f162e47494bde4c26ae68c33514606a7622db
sindydwns <sindydwns@naver.com>
2023-07-28 14:34:01 +0900

    refactor: move parse_vector3, parse_rgb

--- src/Element/common.c
+++ src/Element/common.c
@@ -6,7 +6,7 @@
 /*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 04:09:51 by minjungk          #+#    #+#             */
-/*   Updated: 2023/07/12 08:36:43 by yonshin          ###   ########.fr       */
+/*   Updated: 2023/07/28 14:32:15 by yonshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,59 +17,6 @@
 #include "Element/Plane/plane.h"
 #include "Element/Sphere/sphere.h"
 #include "Element/Cylinder/cylinder.h"
-
-static char	*next_rgb(int *buf, char *curr)
-{
-	if (curr == NULL || ft_isdigit(curr[0]) == 0)
-		return (NULL);
-	*buf = ft_atoi(curr);
-	if (!(0 <= *buf && *buf <= 255))
-		return (NULL);
-	while (ft_isdigit(curr[0]))
-		++curr;
-	if (curr[0] != '\0' && ft_strchr(",\r\n", curr[0]) == NULL)
-		return (NULL);
-	return (curr);
-}
-
-int	parse_rgb(struct s_rgb *rgb, char *curr)
-{
-	curr = next_rgb(&rgb->r, curr);
-	if (curr == NULL || curr[0] != ',')
-		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
-	curr = next_rgb(&rgb->g, curr + 1);
-	if (curr == NULL || curr[0] != ',')
-		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
-	curr = next_rgb(&rgb->b, curr + 1);
-	if (curr == NULL || (curr[0] != '\0' && ft_strchr("\r\n", curr[0]) == NULL))
-		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
-	return (EXIT_SUCCESS);
-}
-
-int	parse_vector3(t_vector3 *vector, char *curr, int scope)
-{
-	double	axis_len;
-
-	vector->x = ft_strtof(curr, &curr);
-	if (errno || curr[0] != ',')
-		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
-	vector->y = ft_strtof(curr + 1, &curr);
-	if (errno || curr[0] != ',')
-		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
-	vector->z = ft_strtof(curr + 1, &curr);
-	if (errno || (curr[0] != '\0' && ft_strchr("\r\n", curr[0]) == NULL))
-		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
-	if (scope == AllScope)
-		return (EXIT_SUCCESS);
-	if (!((-1.0 <= vector->x && vector->x <= 1.0)
-			&& (-1.0 <= vector->y && vector->y <= 1.0)
-			&& (-1.0 <= vector->z && vector->z <= 1.0)))
-		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
-	axis_len = v3_magnitude(*vector);
-	if (!(0.9999 < axis_len && axis_len < 1.0001))
-		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
-	return (EXIT_SUCCESS);
-}
 
 void	init_element(void *elem)
 {
---
+++ src/file/parse_vector3.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_vector3.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/28 14:30:01 by yonshin           #+#    #+#             */
+/*   Updated: 2023/07/28 14:30:28 by yonshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "file_private.h"
+
+int	parse_vector3(t_vector3 *vector, char *curr, int scope)
+{
+	double	axis_len;
+
+	vector->x = ft_strtof(curr, &curr);
+	if (errno || curr[0] != ',')
+		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
+	vector->y = ft_strtof(curr + 1, &curr);
+	if (errno || curr[0] != ',')
+		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
+	vector->z = ft_strtof(curr + 1, &curr);
+	if (errno || (curr[0] != '\0' && ft_strchr("\r\n", curr[0]) == NULL))
+		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
+	if (scope == AllScope)
+		return (EXIT_SUCCESS);
+	if (!((-1.0 <= vector->x && vector->x <= 1.0)
+			&& (-1.0 <= vector->y && vector->y <= 1.0)
+			&& (-1.0 <= vector->z && vector->z <= 1.0)))
+		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
+	axis_len = v3_magnitude(*vector);
+	if (!(0.9999 < axis_len && axis_len < 1.0001))
+		return (ft_error(__func__, __FILE__, __LINE__, EINVAL));
+	return (EXIT_SUCCESS);
+}
