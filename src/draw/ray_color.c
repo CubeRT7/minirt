f692b11a3528770227821ecc0dc4ab3f157915e8
iijung <987kmj4@naver.com>
2023-07-29 23:08:46 +0900

    fix: ambient once

--- src/draw/ray_color.c
+++ src/draw/ray_color.c
@@ -6,7 +6,7 @@
 /*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 07:27:05 by yonshin           #+#    #+#             */
-/*   Updated: 2023/07/29 22:47:45 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/07/29 23:02:17 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,26 +36,19 @@ static t_color	_bright(t_ray ray, t_world *world, t_hit rec, t_light *light)
 {
 	t_ray	to_light;
 	t_range	distance;
-	t_color	ambient;
 	double	brightness;
 
-	ambient = v3_preset(V3_ZERO);
-	if (world->render_mode & RENDER_AMBIENT)
-		ambient = v3_mul(
-				world->ambient_light->base.color,
-				world->ambient_light->ratio);
 	to_light = (t_ray){rec.p, v3_sub(light->base.position, rec.p)};
 	distance = (t_range){DELTA, v3_magnitude(to_light.direction)};
 	to_light.direction = v3_normalize(to_light.direction);
 	if (hit(world->objs, &to_light, distance, NULL))
-		return (ambient);
+		return (v3_preset(V3_ZERO));
 	brightness = 0;
 	if (world->render_mode & RENDER_DIFFUSE)
 		brightness = fmax(0, v3_dot(to_light.direction, rec.normal));
 	if (world->render_mode & RENDER_SPECULAR)
 		brightness += _calc_specular(rec, ray, to_light);
-	return ((v3_add(ambient,
-				v3_mul(light->base.color, light->ratio * brightness))));
+	return (v3_mul(light->base.color, light->ratio * brightness));
 }
 
 t_color	ray_color(t_ray *ray, t_world *world)
@@ -68,6 +61,10 @@ t_color	ray_color(t_ray *ray, t_world *world)
 	if (hit(world->objs, ray, (t_range){DELTA, BIGVALUE}, &rec) == 0)
 		return (v3_preset(V3_ZERO));
 	brightness_sum = v3_preset(V3_ZERO);
+	if (world->render_mode & RENDER_AMBIENT)
+		brightness_sum = v3_mul(
+				world->ambient_light->base.color,
+				world->ambient_light->ratio);
 	curr = world->lights;
 	while (curr)
 	{
