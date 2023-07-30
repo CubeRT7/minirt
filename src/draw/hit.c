95a11803233f40b4295da1eb3ff984ae9ac33e79
iijung <987kmj4@naver.com>
2023-07-30 19:54:10 +0900

    refactor: unncessary max_len range

--- src/draw/hit.c
+++ src/draw/hit.c
@@ -6,7 +6,7 @@
 /*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 04:11:34 by yonshin           #+#    #+#             */
-/*   Updated: 2023/07/28 15:02:24 by yonshin          ###   ########.fr       */
+/*   Updated: 2023/07/30 19:51:58 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,17 @@
 static int	_hit(t_list *objs, t_ray *ray, t_range range, t_hit *record)
 {
 	t_hit		tmp_rec;
-	double		max_len;
 	int			hit_anything;
 	t_element	*elem;
 
 	hit_anything = 0;
-	max_len = range.max;
 	while (objs)
 	{
 		elem = objs->content;
-		if (elem->func[Hit](elem, ray, (t_range){range.min, max_len}, &tmp_rec))
+		if (elem->func[Hit](elem, ray, range, &tmp_rec))
 		{
 			hit_anything = 1;
-			max_len = tmp_rec.t;
+			range.max = tmp_rec.t;
 			tmp_rec.elem = (void *)elem;
 			if (record != NULL)
 				*record = tmp_rec;
