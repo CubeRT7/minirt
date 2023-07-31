2a09187ffeb935507408273bf58ed34737da0d70
sindy <sindydwns@naver.com>
2023-08-01 00:13:37 +0900

    refactor: cylinder alias x (#70)

--- src/Element/Cylinder/hit_cylinder.c
+++ src/Element/Cylinder/hit_cylinder.c
@@ -12,6 +12,22 @@
 
 #include "cylinder.h"
 
+static t_abc	_calc_abc(
+	t_cylinder *self,
+	t_vector3 w,
+	t_vector3 direction,
+	t_vector3 axis)
+{
+	const double	da = v3_dot(direction, axis);
+	const double	wa = v3_dot(w, axis);
+
+	return ((t_abc){
+		v3_dot(direction, direction) - da * da,
+		v3_dot(direction, w) - (da * wa),
+		v3_dot(w, w) - (wa * wa) - (self->r_pow_2)
+	});
+}
+
 static int	hit_circle(t_circle circ, t_ray *ray, t_range range, t_hit *record)
 {
 	double		d;
@@ -37,29 +53,24 @@ static int	hit_circle(t_circle circ, t_ray *ray, t_range range, t_hit *record)
 
 static int	hit_body(t_cylinder *self, t_ray *ray, t_range range, t_hit *record)
 {
-	const double			half_height = self->height * 0.5;
-	t_cylinder_body_alias	alias;
-	t_abc					abc;
-	t_hit					rec;
+	const t_abc	abc = _calc_abc(
+			self, v3_sub(ray->origin, self->base.position),
+			ray->direction, self->base.front);
+	double		t;
+	double		h;
+	t_vector3	p;
+	t_vector3	in;
 
-	alias.r2 = self->radius * self->radius;
-	alias.w = v3_sub(ray->origin, self->base.position);
-	alias.vh = v3_dot(ray->direction, self->base.front);
-	alias.wh = v3_dot(alias.w, self->base.front);
-	abc = (t_abc){
-		v3_dot(ray->direction, ray->direction) - alias.vh * alias.vh,
-		v3_dot(ray->direction, alias.w) - (alias.vh * alias.wh),
-		v3_dot(alias.w, alias.w) - (alias.wh * alias.wh) - (alias.r2)
-	};
-	if (quadratic_formula_root(abc, range, &(rec.t)))
+	if (quadratic_formula_root(abc, range, &t))
 		return (0);
-	rec.p = get_ray_point(ray, rec.t);
-	alias.l = v3_dot(v3_sub(rec.p, self->base.position), self->base.front);
-	if (range_not_in(alias.l, (t_range){-half_height, half_height}))
+	p = get_ray_point(ray, t);
+	h = v3_dot(v3_sub(p, self->base.position), self->base.front);
+	if (range_not_in(h, (t_range){-self->half_height, self->half_height}))
 		return (0);
-	ft_memcpy(record, &rec, sizeof(t_hit));
-	alias.in = v3_add(self->base.position, v3_mul(self->base.front, alias.l));
-	record->normal = v3_normalize(v3_sub(record->p, alias.in));
+	record->t = t;
+	record->p = p;
+	in = v3_add(self->base.position, v3_mul(self->base.front, h));
+	record->normal = v3_normalize(v3_sub(record->p, in));
 	record->normal = get_face_normal(*ray, record->normal);
 	return (1);
 }
