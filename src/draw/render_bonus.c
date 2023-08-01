e200557e6d8bfc4132396517faf3be311a168008
sindy <sindydwns@naver.com>
2023-08-01 13:42:39 +0900

    Feat/thread rendering (#72)
    * feat: thread rendering
    
    * feat: +antialiasing - high quality low frame
    
    * feat: remove antialiasing from mandatory

--- src/draw/render_bonus.c
+++ src/draw/render_bonus.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   render.c                                           :+:      :+:    :+:   */
+/*   render_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yonshin <yonshin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,17 +12,7 @@
 
 #include "miniRT.h"
 
-static void	_update_next_render_idx(t_device *device)
-{
-	device->separated_render_curr.x++;
-	if (device->separated_render_curr.x >= device->separated_render_max.x)
-	{
-		device->separated_render_curr.x = 0;
-		device->separated_render_curr.y++;
-	}
-	if (device->separated_render_curr.y >= device->separated_render_max.y)
-		device->separated_render_curr.y = 0;
-}
+void	render_image(t_world *world);
 
 static void	_render_text(t_world *world)
 {
@@ -48,35 +38,12 @@ static void	_render_text(t_world *world)
 	put_str(device, vector3(10, 30, 0), edit, len);
 }
 
-static void	_render_image(t_world *world)
-{
-	t_device *const	device = &world->device;
-	t_vector3		pos;
-	int				pixel_color;
-
-	pos.x = device->separated_render_curr.x;
-	while (pos.x < device->size.x)
-	{
-		pos.y = device->separated_render_curr.y;
-		while (pos.y < device->size.y)
-		{
-			if (pos.x > device->size.x || pos.y > device->size.y)
-				break ;
-			pixel_color = color_to_pixel(ray_color(world, pos));
-			put_pixel(device, pos.x, device->size.y - pos.y - 1, pixel_color);
-			pos.y += device->separated_render_max.y;
-		}
-		pos.x += device->separated_render_max.x;
-	}
-	_update_next_render_idx(device);
-}
-
 void	render(t_world *world)
 {
 	t_device *const		device = &world->device;
 
 	mlx_clear_window(device->mlx, device->win);
-	_render_image(world);
+	render_image(world);
 	_render_text(world);
 	mlx_put_image_to_window(device->mlx, device->win, device->img, 0, 0);
 }
